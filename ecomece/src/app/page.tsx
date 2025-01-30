0b76c0e637376c7ec75bbf23720046ecaa60e07e
Danilo Souza <danilobrayann@gmail.com>
2025-01-30 14:46:09 -0300

    feat: list new product

--- ecomece/src/app/page.tsx
+++ ecomece/src/app/page.tsx
@@ -1,9 +1,30 @@
 
 export default function Home() {
   return (
-   <main>
-   <h1> hello</h1>
-   
-   </main>
+   <div className="max-w-7xl mx-auto px-8 sm:px-8 lg:px-0">
+   <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
+      prod 1
+    </div>
+
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
+      prod 1
+    </div>
+    
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
+      prod 1
+    </div>
+    
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
+      prod 1
+    </div>
+    
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
+      prod 1
+    </div>
+    
+   </div>
+   </div>
   )
 }
