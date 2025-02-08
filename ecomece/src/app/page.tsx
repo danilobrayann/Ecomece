5b47d5d3a6b6a9a2903d1ac3307de3a4a3cd84be
Danilo Souza <danilobrayann@gmail.com>
2025-02-08 15:41:35 -0300

    feat: type products

--- ecomece/src/app/page.tsx
+++ ecomece/src/app/page.tsx
@@ -1,20 +1,27 @@
-interface product {
-  id: number;
-  title: string;
-}
+import { ProductType } from "@/types/productstype";
+import Image from "next/image";
+
 async function getProducts() {
   const data = await fetch("https://fakestoreapi.com/products");
   return data.json();
 }
 
 export default async function Home() {
-  const productsList: product[] = await getProducts();
+  const productsList = await getProducts();
 
   return (
     <div className="max-w-7xl mx-auto px-8 sm:px-8 lg:px-0">
       <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        {productsList.map((product) => (
-          <div key={product.id}>{product.title}</div>
+        {productsList.map((product: ProductType) => (
+          <div key={product.id}>
+            {product.title}
+            <Image
+              src={product.image}
+              alt={product.title}
+              width={100}
+              height={100}
+            />
+          </div>
         ))}
       </div>
     </div>
---
+++ ecomece/src/types/productstype.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+  id: number;
+  price: number | null;
+  description: string | null;
+  title: string;
+  image: string;
+  category: string;
+};
+
