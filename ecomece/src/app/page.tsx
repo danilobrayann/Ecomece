1d0ffea7749a2fc4100b8fee7d189888e8bf43fb
Danilo Souza <danilobrayann@gmail.com>
2025-02-08 12:07:59 -0300

    feat: list product and api

--- ecomece/src/app/page.tsx
+++ ecomece/src/app/page.tsx
@@ -1,30 +1,22 @@
+interface product {
+  id: number;
+  title: string;
+}
+async function getProducts() {
+  const data = await fetch("https://fakestoreapi.com/products");
+  return data.json();
+}
 
-export default function Home() {
+export default async function Home() {
+  const productsList: product[] = await getProducts();
+
   return (
-   <div className="max-w-7xl mx-auto px-8 sm:px-8 lg:px-0">
-   <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
-      prod 1
+    <div className="max-w-7xl mx-auto px-8 sm:px-8 lg:px-0">
+      <div className="grid grid-cols-1  sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {productsList.map((product) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
+      </div>
     </div>
-
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
-      prod 1
-    </div>
-    
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
-      prod 1
-    </div>
-    
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
-      prod 1
-    </div>
-    
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden"> 
-      prod 1
-    </div>
-    
-   </div>
-   </div>
-  )
+  );
 }
