540c55a3887bb164ddf742755ea57c1c4de44124
DaniloBrayann <DaniloBrayann@gmail.com>
2025-09-01 14:49:06 -0300

    feat: adicionar componente ProductsImagem para exibição de imagens de produtos

--- ecomece/src/components/products.tsx
+++ ecomece/src/components/products.tsx
@@ -1,5 +1,5 @@
 import { ProductType } from "@/types/productstype";
-
+import ProductsImagem from "./productsimagem";
 type ProductProps = {
   product: ProductType;
 };
@@ -7,8 +7,13 @@ type ProductProps = {
 export default function Product({ product }: ProductProps) {
   return (
     <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 text-gray-300">
-      <div className="relative max-h-72 flex-1">{product.image}</div>
-      <div className="flex justify-between font-bold my-3">{product.title}</div>
+      <div className="relative max-h-72 flex-1">
+        <ProductsImagem product={product} fill />
+      </div>
+      <div className="flex justify-between font-bold my-3">
+        <p className="w-40 truncate">{product.title}</p>
+        <p className="text-md text-teal-300">{product.price}</p>
+      </div>
       <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
         Adicionar ao meu carrinho
       </button>
---
+++ ecomece/src/components/productsimagem.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+import { ProductType } from "@/types/productstype";
+import Image from "next/image";
+
+type productimagemProps = {
+  product: ProductType;
+  fill?: boolean;
+};
+export default function ProductsImagem({ product, fill }: productimagemProps) {
+  const [loading, setLoading] = useState(true);
+  return fill ? (
+    <Image
+      src={product.image}
+      fill={true}
+      alt={product.title}
+      className={`object-cover ${
+        loading
+          ? "scale-110 blur-3xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+      }`}
+      onLoadingComplete={() => setLoading(false)}
+    />
+  ) : (
+    <Image src={product.image}
+      width={400}
+      height={700}
+      alt={product.title}
+      className={`object-cover ${
+        loading
+          ? "scale-110 blur-3xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+      }`}
+      onLoadingComplete={() => setLoading(false)} />
+  );
+}
